d315343d9ec6607d41078c898ec9872581df2a09
ivelthoven <ivovelthoven@hotmail.com>
2019-06-05 19:44:07 +0200

    More responsive, About page added, fade-in.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+// Initialize Particles.js
 particlesJS.load('particles-js', './particles.json', function() {
     console.log('particles.js loaded..');
 });
+
 
 const typeEffect = (el, lineEnabled) => {
     const txt = el.html();
@@ -13,57 +15,55 @@ const typeEffect = (el, lineEnabled) => {
         for (let i = 0; i < txt.length; i++) {
             setTimeout(() => {
                 if (el.html()[el.html().length - 1] === line) {
-                    el.html(el.html().substr(0, el.html().length-1));
+                    el.html(el.html().substr(0, el.html().length - 1));
                 }
                 el.append(txt.charAt(i) + line)
-                if (i === txt.length - 1){
+                if (i === txt.length - 1) {
                     res();
                     if (!(lineEnabled === false)) {
                         setInterval(() => {
                             if (el.html()[el.html().length - 1] === line) {
-                                el.html(el.html().substr(0, el.html().length-1));
+                                el.html(el.html().substr(0, el.html().length - 1));
                             } else {
                                 el.append(line);
                             }
                         }, 500)
                     }
                 }
-            }
-            , i * 150);
+            }, i * 150);
         }
     })
 };
 
+const checkFadeIn = () => {
+    $(".fade-in").each((i, el) => {
+        let bottom_of_object = $(el).offset().top + 100;// 100 to delay fadeIn effect on scroll   if fully on page: + $(el).outerHeight();
+        let bottom_of_window = $(window).scrollTop() + $(window).height();
+        console.log(bottom_of_object, bottom_of_window)
+        if( bottom_of_window > bottom_of_object ){
+            $(el).css("opacity", "1");
+        }
+    })
+}
+
+
 /*
-* INIT
-*/
+ * INIT
+ */
 $(document).ready(() => {
     $("#title_sub").hide();
-    $(".chevron").hide();
-    $("main").hide();
-    $("footer").hide();
     typeEffect($("#title"), false).then(() => {
         $("#title_sub").show();
-        $(".chevron").show();
         typeEffect($("#title_sub"), false);
     });
+
+    checkFadeIn();
+
+    $(window).scroll(() => {
+        checkFadeIn();
+     })
+
+    console.log(`bootstrap ${$.fn.tooltip.Constructor.VERSION} loaded..`);
 });
 
-let scrolled = false;
-$(window).scroll(() => {
-    console.log("SCROLLING")
-    // const px = $(window).scrollTop();
-    if (!scrolled) {
-        scrolled = true;
-        window.scrollTo(0, 0);
-        $("body").css("overflow", "auto");
-        $("main").show();
-        $("footer").show();
 
-        $("header").css("height", "0vh");
-        $("#title").css("opacity", "0");
-        $("#title_sub").css("opacity", "0");
-        $(".particles-js-canvas-el").hide();
-        $(".chevron").hide();
-    }
-})
